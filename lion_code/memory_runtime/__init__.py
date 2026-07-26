febb309698f8daca68eb14451b85dac137ca2b04
zhangcx <2416118206@qq.com>
2026-07-26 17:16:59 +0800

    refactor: 增加供应商无关的记忆查询契约

---
+++ lion_code/memory_runtime/__init__.py
@@ -0,0 +1,11 @@
+"""Memory 召回在 Core Runtime 中的临时上下文投影。"""
+
+from lion_code.memory_runtime.query import (
+    LegacySdkTextQueryService,
+    TextQueryService,
+)
+
+__all__ = [
+    "LegacySdkTextQueryService",
+    "TextQueryService",
+]
